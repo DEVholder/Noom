bcad9b7151d9a0191ae7c34895f2c195479dedd3
DEVholder <dev0holder@gmail.com>
2023-01-16 14:01:24 +0900

    realtime chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,41 @@
+const messageList = document.querySelector("ul");
+const nickform = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () =>{
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handlesOpen(){
     console.log("Connected to Server");
-})
+}
+socket.addEventListener("open", handlesOpen);
 
 socket.addEventListener("message", (message) => {
-    console.log("just got this:", message.data, "from the server");
-})
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
 
 socket.addEventListener("close", () =>{
     console.log("Disconnected from Server");
-})
+});
 
-setTimeout(() => {
-    socket.send("hello from browser!");
-}, 5000);
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickform.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    // input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickform.addEventListener("submit", handleNickSubmit);
